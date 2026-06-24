4a051b708c54809c2abc4673cc2401519e26b439
SangTran <tranphuocsang181004@gmail.com>
2026-06-24 10:02:04 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,9 +1,1 @@
-### 1. CÁC THAM SỐ CẤU HÌNH ###
-REAL_PATH = "/kaggle/input/faceforensics/FF++/real"
-FAKE_PATH = "/kaggle/input/faceforensics/FF++/fake"
-OUTPUT_FRAME_SIZE = (224, 224)
-FRAME_COUNT = 15
-MAX_VIDEOS = 700
-BATCH_SIZE = 8
-PADDING_FACTOR = 1.3
-LR_FINE_TUNE = 1e-6 # Learning Rate ban đầu cho Fine-Tuning
+
